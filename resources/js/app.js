6f1ebe1c50a3999f3302c41157f8b16bb58adb90
YUU-KUN <indra_wahyu_27rpl@student.smktelkom-mlg.sch.id>
2021-08-18 10:22:25 +0700

    add bootstrap vue, change bootstrap version, edit styling

--- resources/js/app.js
+++ resources/js/app.js
@@ -13,6 +13,7 @@ import VueAxios from 'vue-axios';
 import VueRouter from 'vue-router';
 import axios from 'axios';
 import { routes } from './routes';
+import BootstrapVue from 'bootstrap-vue'
 
 // CSS
 import '../../public/assets/css/bootstrap.css'
@@ -31,8 +32,21 @@ import '../../public/assets/css/owl-carousel.css'
 import '../../public/assets/css/niceselect.css'
 import '../../public/assets/css/slicknav.min.css'
 
+// import '../../public/assets/js/jquery.min.js'
+// import '../../public/assets/js/active.js'
+// import '../../public/assets/js/easing.js'
+// import '../../public/assets/js/flex-slider.js'
+// import '../../public/assets/js/jquery-migrate-3.0.0.js'
+// import '../../public/assets/js/magnific-popup.js'
+// import '../../public/assets/js/map-script.js'
+// import '../../public/assets/js/owl-carousel.js'
+// import '../../public/assets/js/scrollup.js'
+
+// import '../../public/assets/js/nicesellect.js'
+
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.use(VueRouter)
+Vue.use(BootstrapVue)
 Vue.use(VueAxios, axios)
 
  const router = new VueRouter({
