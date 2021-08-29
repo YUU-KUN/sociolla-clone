26b050f803108e887e6c287dbd4ad33a107f25df
YUU-KUN <indra_wahyu_27rpl@student.smktelkom-mlg.sch.id>
2021-08-30 00:32:51 +0700

    seeders

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'username' => 'superadmin',
+            'email' => 'superadmin@gmail.com',
+            'password' => 'secret123',
+            'level' => 'superadmin'
+        ]);
+    }
+}
