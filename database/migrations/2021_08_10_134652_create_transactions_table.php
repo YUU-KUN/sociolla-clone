77bfc8be9eb9501bc6d74e6b98e37dfa07c454c1
YUU-KUN <indra_wahyu_27rpl@student.smktelkom-mlg.sch.id>
2021-08-31 13:09:59 +0700

    migrations

--- database/migrations/2021_08_10_134652_create_transactions_table.php
+++ database/migrations/2021_08_10_134652_create_transactions_table.php
@@ -15,12 +15,25 @@ class CreateTransactionsTable extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('product_id');
-            $table->integer('quantity');
+            // $table->unsignedBigInteger('user_id');
+            // $table->unsignedBigInteger('cart_id');
+            // $table->unsignedBigInteger('checkout_id');
+            $table->string('address');
+            $table->string('province_id');
+            $table->string('regency_id');
+            $table->string('district_id');
+            $table->string('village_id');
+            $table->string('payment_image');
             $table->integer('total');
-            $table->enum('status', ['pending', 'approved', 'completed']);
+            $table->enum('status', ['pending', 'approved', 'completed'])->default('pending');
             $table->timestamps();
+
+            $table->foreign('province_id')->references('id')->on('provinces')->onDelete('cascade');
+            $table->foreign('regency_id')->references('id')->on('regencies')->onDelete('cascade');
+            $table->foreign('district_id')->references('id')->on('districts')->onDelete('cascade');
+            $table->foreign('village_id')->references('id')->on('villages')->onDelete('cascade');
+            // $table->foreign('checkout_id')->references('id')->on('checkouts')->onDelete('cascade');
+
         });
     }
 
